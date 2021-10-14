854b707e1e2f7e664a0584d7cdf603694db95b06
Adrian Filpescu <adrian.filipescu@gmail.com>
2021-10-14 15:54:49 +0300

    Finance

--- src/main/java/Finance.java
+++ src/main/java/Finance.java
@@ -1,9 +1,12 @@
 
 public class Finance {
-    
+
+    public final static String BEST_LOAN_RATES = "bestLoanRates";
+    public final static String SAVINGS_CALCULATOR = "savingsCalculator";
+    public final static String MORTGAGE_CALCULATOR = "mortgageCalculator";
 
     public static void main(String[] args) {
-        
+
     }
 
 }
