49717b22b1c99375df92a5cf1464ec748dc0f6ee
Adrian Filpescu <adrian.filipescu@gmail.com>
2021-10-18 16:56:34 +0300

    Utilities

--- src/main/java/com/h2/Utilities.java
+++ src/main/java/com/h2/Utilities.java
@@ -32,4 +32,18 @@ public class Utilities {
     }
 
 
+    public static float getFloatValue(String in){
+
+        float out = Float.MIN_VALUE;
+
+        try {
+            out = Float.parseFloat(in);
+            
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(in + " cannot be converted into a 'float' value. Exiting program.");
+        }
+        return out;
+    }
+
+
 }
